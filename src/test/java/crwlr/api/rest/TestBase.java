f46c99cf7167117dbaa095c2b31811aca24aba28
HaoHo <haho@codix.eu>
2017-10-22 09:22:57 +0700

    Removing spring-mvc-test

--- src/test/java/crwlr/api/rest/TestBase.java
+++ src/test/java/crwlr/api/rest/TestBase.java
@@ -1,12 +1,9 @@
 package crwlr.api.rest;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.io.IoBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.MessageSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -17,20 +14,18 @@ import org.testng.annotations.BeforeClass;
 
 import java.io.PrintWriter;
 import java.net.UnknownHostException;
-import java.util.Locale;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 /**
  * Created by haho
- * Date:  05/05/2016 Time: 5:15 PM
+ * Date:  10/19/2017 Time: 5:15 PM
  */
 @WebAppConfiguration
 @ContextConfiguration(
     locations = {
-        "/config/spring-mvc.xml",
-        "/config/spring-mvc-test.xml"
+        "/config/spring-mvc.xml"
     })
 public abstract class TestBase extends AbstractTestNGSpringContextTests {
   protected final Logger logger = LogManager.getLogger(getClass());
@@ -38,17 +33,7 @@ public abstract class TestBase extends AbstractTestNGSpringContextTests {
   protected MockMvc mockMvc;
 
   @Autowired
-  @Qualifier("testObjectMapper")
-  protected ObjectMapper objectMapper;
-
-  @Autowired
   private WebApplicationContext wac;
-
-  @Autowired
-  @Qualifier("tstMsgSource")
-  private MessageSource messageSource;
-
-  public static Locale locale = new Locale("en");
 
   @BeforeClass
   public void setup() throws UnknownHostException {
